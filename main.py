3aaca312c9a4b743509c32eb141872edf040d39b
kmirek1 <karissa.mirek@gmail.com>
2025-11-20 14:54:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-# Karissa Mirek, Lizbeth Moran, Jocelyn Alvarez-Martinez
+# Karissa Mirek, Lizbeth Moran, Jocelyn Alvarez-Martinez, Alexis Salazar
+
 students=[" Jeremy", " Stacy", " Jenny", " Candace", " Ferb", " Candace"] #defines the list of students and the times that they have been out of the classroom 
 minutes_out=[7, 10, 15, 8, 3, 20]#defines the list of the minutes each students were out of the classroom
 
@@ -20,10 +21,10 @@ statuses.append(status) #adds append to the list
 notes.append(note) #adds note to the list 
 
 print(f"Student:{name}") #prints "Student:{name of one of the students}" 
-print(f"Time out:{mins} minutes") #prints "Time out:{mins out of the classroom} minutes"
-# print(f"Status:{status}") #prints "Status:{status of the student}"
+print(f"Time out:{mins} minutes") #prints "Time out:{mins out oft}"
 
-if note: #sets condition for "note"
+if note: #sets condition for "note" the classroom} minutes"
+# print(f"Status:{status}") #prints "Status:{status of the studen
     print(f"Note:{note}") # prints Note: {notes of the students}
 print () #prints "duplicate name detceted"
 
@@ -31,6 +32,6 @@ print () #prints "duplicate name detceted"
 average_time=sum(minutes_out)/len(minutes_out) #calculates the aveerage time student was in hallway
 print("Average time in hallways: ", average_time)  # prints out the average time student spent in hallway
 
-if average_time>=10:
-    status="FLAGGED"
-print(status)
+if average_time>=10: #sets condition for average_time
+    status="FLAGGED" #sets status as the value "FLAGGED"
+print(status) #prints "(status)"
